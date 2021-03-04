4b15494277236f73f807322562d28d431a30e0c7
CamiYanez <ccynzz@gmail.com>
2021-03-04 02:41:42 -0500

    Auto-generated commit

--- src/app/Interfaces/menu.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Menu {
-  id: string;
-  item: string;
-  price: number;
-  category: string;
-  time: string[];
-}
--- src/app/Interfaces/order.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Order {
-  name: string;
-  table: number;
-  bill: Number;
-  items: Array<string>;
-  createdAt: Date;
-  state: string;
-}
